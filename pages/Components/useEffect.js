938d68535b046b846fb8ef5763ded288581c1ffa
rayhan <prog.rayhan1@gmail.com>
2022-04-02 11:40:25 +0600

    useEffect hook added

--- pages/Components/useEffect.js
+++ pages/Components/useEffect.js
@@ -1,9 +1,33 @@
-import React from 'react'
+import { useState, useEffect } from "react";
 
-function useEffect() {
-  return (
-    <div>This is useEffect</div>
+function App() {
+  const [number,setNumber] = useState(0)
+  const [number2,setNumber2] = useState(0)
+
+  useEffect(()=>{
+    console.log(`I am click ${number} times`);
+  })
+
+  useEffect(()=>{
+    console.log(`I am click ${number2}`);
+  })
+
+  const changeButton1 =()=>{
+    setNumber(number+1)
+  }
+
+  const changeButton2=()=>{
+    setNumber2(number2+1)
+  }
+
+  return(
+    <div>
+      <p>Number is {number}</p>
+      <button onClick={changeButton1}>Button 1</button>
+      <p>Number is {number2}</p>
+      <button onClick={changeButton2}>Button 2</button>
+    </div>
   )
 }
 
-export default useEffect
+export default App;
