bd392e048a3ea6fa34f3a3fe5141b8c7718c0963
rayhan <prog.rayhan1@gmail.com>
2022-04-03 10:43:44 +0600

    useForm hook added

--- pages/Components/useForm.js
+++ pages/Components/useForm.js
@@ -1,9 +1,55 @@
-import React from 'react'
+// import React from "react";
+// import { useForm } from "react-hook-form";
 
-function useForm() {
+// const RegisterForm = () => {
+//   const { register, handleSubmit } = useForm();
+//   const handleRegistration = (data) => console.log(data);
+
+//   return (
+//     <form onSubmit={handleSubmit(handleRegistration)}>
+//       <div>
+//         <label>Name</label>
+//         <input name="name" {...register('name')} />
+//       </div>
+//       <div>
+//         <label>Email</label>
+//         <input type="email" name="email" {...register('email')} />
+//       </div>
+//       <div>
+//         <label>Password</label>
+//         <input type="password" name="password" {...register('password')} />
+//       </div>
+//       <button>Submit</button>
+//     </form>
+//   );
+// };
+// export default RegisterForm;
+
+import React from "react";
+import { useForm } from "react-hook-form";
+
+const rayhan = () => {
+  const { register, handleSubmit } = useForm();
+  const handleRegistration = (data) => console.log(data)
   return (
-    <div>I am useForm</div>
+    <form onSubmit={handleSubmit(handleRegistration)}>
+      <div>
+        <label>Enter your name</label>
+        <input name="firstName" {...register('firstName')} placeholder="Name"></input>
+      </div>
+
+      <div>
+        <label>Enter your Email</label>
+        <input type="email" name="email" {...register('email')} placeholder="Email"></input>
+      </div>
+
+      <div>
+        <label>Enter Your Password</label>
+        <input type="password" name="password" {...register("password")} placeholder="Password"></input>
+      </div>
+      <button>Submit</button>
+    </form>
   )
 }
 
-export default useForm
+export default rayhan
